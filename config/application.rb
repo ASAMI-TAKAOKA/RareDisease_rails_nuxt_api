23dd7645a87b64960406c850a9d9aa748311012b
Asami Takaoka <asamiyamabe61@gmail.com>
2022-10-26 23:19:27 +0900

    Initialized application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,10 @@ module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    # 追加
+    # Railsアプリのタイムゾーン(default 'UTC')
+    # TimeZoneList: http://api.rubyonrails.org/classes/ActiveSupport/TimeZone.html
+    config.time_zone = ENV["TZ"]
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -35,6 +39,19 @@ module App
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+
+    # 追加
+    # データベースの読み書きに使用するタイムゾーン(:local | :utc(default))
+    config.active_record.default_timezone = :utc
+
+    # 追加
+    # i18nで使われるデフォルトのロケールファイルの指定(default :en)
+    config.i18n.default_locale = :ja
+
+    # 追加
+    # $LOAD_PATHにautoload pathを追加しない(Zeitwerk有効時false推奨)
+    config.add_autoload_paths_to_load_path = false
+
     config.api_only = true
   end
 end
