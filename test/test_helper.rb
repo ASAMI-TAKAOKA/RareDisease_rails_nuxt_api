bc261e5b0f147016895c4f9c1dc16076f5fa05eb
Asami Takaoka <asamiyamabe61@gmail.com>
2022-11-04 14:32:35 +0900

    Setup Test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,16 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
+# gem 'minitest-reporters' setup
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
+  parallelize_setup do |worker|
+    # seedデータの読み込み
+    load "#{Rails.root}/db/seeds.rb"
   # Run tests in parallel with specified workers
+  end
+
   parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 end
